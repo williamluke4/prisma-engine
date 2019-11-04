3709b701ae6a1a7d8e984cf5092dc20664283d48
Tom Houlé <tom@tomhoule.com>
2019-11-04 11:14:52 +0100

    Implement serialization of user facing errors

--- libs/user-facing-errors/src/lib.rs
+++ libs/user-facing-errors/src/lib.rs
@@ -4,8 +4,33 @@ pub mod common;
 pub mod introspection;
 pub mod migration_engine;
 
-pub trait UserFacingError {
+use serde::Serialize;
+
+pub trait UserFacingError: serde::Serialize {
     const ERROR_CODE: &'static str;
 
     fn message(&self) -> String;
+}
+
+#[derive(Serialize)]
+pub struct KnownError {
+    message: String,
+    meta: serde_json::Value,
+    error_code: &'static str,
+}
+
+impl KnownError {
+    pub fn new<T: UserFacingError>(inner: T) -> Result<KnownError, serde_json::Error> {
+        Ok(KnownError {
+            message: inner.message(),
+            meta: serde_json::to_value(&inner)?,
+            error_code: T::ERROR_CODE,
+        })
+    }
+}
+
+#[derive(Serialize)]
+pub struct UnknownError {
+    pub message: String,
+    pub backtrace: Option<String>,
 }
